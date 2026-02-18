c04d97f68e855be1a9a2ed6f72f7e9f9901a5b3e
Ayesha Adil <ayeshalovesadil@gmail.com>
2026-02-18 15:34:46 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6409784031-be7a8",
+  "appId": "1:155086108337:web:0afd89e7b39838ee709490",
+  "apiKey": "AIzaSyCZpANgbKLjNefLYpNZGQLhFgb97_P60xs",
+  "authDomain": "studio-6409784031-be7a8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "155086108337"
+};
