9d2e851cf6925853ff2804feb3540b50c98bc00a
Adil Munawar <adilfromavengers@gmail.com>
2026-02-21 01:22:03 +0000

    instead of such map option you have to show me a pop up like functionali

--- src/lib/map-layers.ts
+++ src/lib/map-layers.ts
@@ -13,7 +13,7 @@ export const mapLayers: MapLayer[] = [
     name: 'Satellite',
     url: 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
     attribution: '&copy; Google Maps',
-    previewImageUrl: 'https://picsum.photos/seed/satellite-view/128/128',
+    previewImageUrl: 'https://picsum.photos/seed/satellite/128/128',
     imageHint: 'satellite view',
   },
   {
@@ -21,7 +21,7 @@ export const mapLayers: MapLayer[] = [
     name: 'Esri Imagery',
     url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
     attribution: '&copy; Esri &mdash; i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
-    previewImageUrl: 'https://picsum.photos/seed/esri-imagery/128/128',
+    previewImageUrl: 'https://picsum.photos/seed/imagery/128/128',
     imageHint: 'satellite imagery',
   },
   {
@@ -29,7 +29,7 @@ export const mapLayers: MapLayer[] = [
     name: 'Street',
     url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     attribution: '&copy; OpenStreetMap contributors',
-    previewImageUrl: 'https://picsum.photos/seed/street-map/128/128',
+    previewImageUrl: 'https://picsum.photos/seed/street/128/128',
     imageHint: 'street map',
   },
   {
@@ -37,7 +37,7 @@ export const mapLayers: MapLayer[] = [
     name: 'Topographic',
     url: 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
     attribution: 'Map data: &copy; OpenStreetMap contributors, SRTM | Map style: &copy; OpenTopoMap (CC-BY-SA)',
-    previewImageUrl: 'https://picsum.photos/seed/topo-map/128/128',
+    previewImageUrl: 'https://picsum.photos/seed/topography/128/128',
     imageHint: 'topographic map',
   },
   {
@@ -45,7 +45,15 @@ export const mapLayers: MapLayer[] = [
     name: 'Light',
     url: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
     attribution: '&copy; OpenStreetMap contributors &copy; CARTO',
-    previewImageUrl: 'https://picsum.photos/seed/light-map/128/128',
+    previewImageUrl: 'https://picsum.photos/seed/lightmap/128/128',
     imageHint: 'light map',
+  },
+  {
+    id: 'carto-dark',
+    name: 'Dark',
+    url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+    attribution: '&copy; OpenStreetMap contributors &copy; CARTO',
+    previewImageUrl: 'https://picsum.photos/seed/darkmap/128/128',
+    imageHint: 'dark map',
   },
 ];
